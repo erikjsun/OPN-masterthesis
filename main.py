d4ede0976dbdcfa5947ae40f4afa7b58383702ce
erikjsun <44667817+erikjsun@users.noreply.github.com>
2023-12-14 11:52:19 +0100

    refactoring code & addnig a new main.py file to allow for multiprocessing

--- main.py
+++ main.py
@@ -1,40 +1,181 @@
 from azure.storage.blob import BlobServiceClient, ContainerClient, BlobPrefix
-from data.py import BlobSamples, PreparedDataset, PreprocessedTemporalFourData
-from data.py import test_video_dataset, test_temporal_four, visualize_frames, visualize_optical_flow
+from data import BlobSamples, PreparedDataset, PreprocessedTemporalFourData
+from data import test_video_dataset, test_temporal_four, visualize_frames, visualize_optical_flow
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim.lr_scheduler import MultiStepLR
+from model import CustomOPN
+import time
 
 def main():
-    STORAGEACCOUNTURL = "https://exjobbssl1863219591.blob.core.windows.net"
-    STORAGEACCOUNTKEY = "PuL1QY8bQvIyGi653lr/9CPvyHLnip+cvsu62YAipDjB7onPDxfME156z5/O2NwY0PRLMTZc86/6+ASt5Vts8w=="
-    CONTAINERNAME = "exjobbssl"
-    FOLDERNAME = "UCF-101/Diving/" #Temporary during the development phase
-    #BLOBNAME = "UCF-101/ApplyEyeMakeup/v_ApplyEyeMakeup_g01_c01.avi"
+    print("loading temporal four dataset")
+    dataset_train = torch.load('dataset_train.pth')
+    train_model(dataset_train)
 
-    blob_service_client_instance = BlobServiceClient(
-        account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
+def train_model(dataset_train):
+    #Initialize the model
+    model = CustomOPN()
+    criterion = nn.CrossEntropyLoss()
 
-    #container_client_instance = blob_service_client_instance.get_container_client(CONTAINERNAME)
+    #Setting optimizer and scheduler
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10, 20], gamma=0.1) #TODO make it milestones=[130000, 170000]
 
-    # Usage:
-    sample = BlobSamples()
+    # Create data loaders
+    train_loader = torch.utils.data.DataLoader(dataset_train, batch_size=32, shuffle=True, num_workers=3)#)
+    # TODO potentially replace with starmap? Or simply forego Jupyter Notebook altogether?
 
-    videos_loaded = 200
-    print('loading videos')
-    videos = sample.load_videos_into_memory(blob_service_client_instance, CONTAINERNAME, FOLDERNAME, videos_loaded)
-    print('videos loaded')
-    print("Amount of videos loaded: " + str(len(videos)))
+    print(f'Number of batches in train_loader: {len(train_loader)}')
 
-    # Initialize the train/test dataset
-    video_dataset = PreparedDataset(videos, trainval='train')
+    # Assuming validation_data is your validation dataset
+    #validation_loader = torch.utils.data.DataLoader(validation_data, batch_size=32, shuffle=False) TODO add validation data
 
-    # Usage:
-    amount_of_videos_to_display = 6
-    test_video_dataset(video_dataset, amount_of_videos_to_display)
+    # Training loop
+    epoch_amount = 10
 
-    # Usage:
-    amount_of_videos_to_load = 6
-    video_dataset = PreparedDataset(videos, trainval='train')
-    temporal_four = PreprocessedTemporalFourData(video_dataset, trainval='train')
-    test_temporal_four(temporal_four, amount_of_videos_to_load)
+    for epoch in range(epoch_amount): # max_iter, TODO make it 17000
+        # Training phase
+        print('Starting Training')
+        model.train()
+        running_loss = 0.0
+        running_corrects = 0
+
+        start_time = time.time()
+        for inputs, frame_order_labels, action_labels, _, _, _ in train_loader:
+            end_time = time.time()
+            print(f'Time taken to retrieve elements: {end_time - start_time} seconds') # EXTREMELY SLOW +27 sec
+            # Convert inputs to float
+            inputs = inputs.to(torch.float32)
+
+            #TODO check if this is correct
+            inputs = inputs.permute(0, 1, 4, 2, 3)  # Change the order of dimensions to (batch_size, frames, channels, height, width)
+            inputs = inputs.contiguous().view(-1, inputs.shape[1]*inputs.shape[2], inputs.shape[3], inputs.shape[4])  # Concatenate frames and channels
+
+            # Zero the parameter gradients
+            optimizer.zero_grad()
+
+            # Forward pass
+            #start_time = time.time()
+            outputs = model(inputs)
+            #end_time = time.time()
+            #print(f'Time taken for forward pass: {end_time - start_time} seconds') #EXTREMELY FAST
+            #print(outputs)
+
+            # Get predicted class labels
+            _, predicted_labels = torch.max(outputs, 1)
+            #print("Predicted labels: ", predicted_labels)
+                    
+            # Calculate loss
+            loss = criterion(outputs, frame_order_labels)
+
+            # Backward pass and optimize
+            #start_time = time.time()
+            loss.backward()
+            #end_time = time.time()
+            #print(f'Time taken for backward pass: {end_time - start_time} seconds') #EXTREMELY FAST
+            optimizer.step()
+
+            # Statistics
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(predicted_labels == frame_order_labels.data)
+
+        epoch_loss = running_loss / len(train_loader.dataset)
+        epoch_acc = running_corrects.double() / len(train_loader.dataset)
+
+        print(f'Epoch {epoch}/{epoch_amount-1}, Loss: {epoch_loss}, Accuracy: {epoch_acc}')
+        
+        # Every 10000 iterations, save a snapshot of the model
+        #if epoch % 10000 == 0:
+        # At the end of each epoch, save the model's weights
+        torch.save(model.state_dict(), f'model_epoch_{epoch}.pt')
+
+        # TODO add validation phase to possibly fine-tune the model's parameters.
+        # Validation phase
+        #model.eval()
+        #correct = 0
+        #total = 0
+        #with torch.no_grad():
+        #    for inputs, frame_labels, video_labels in validation_loader:
+        #        # Convert inputs to float
+        #        inputs = inputs.to(torch.float32)p
+        #        outputs = model(inputs)
+        #        _, predicted = torch.max(outputs.data, 1)
+        #        total += frame_labels.size(0)
+        #        correct += (predicted == frame_labels).sum().item()
+        #print(f'Epoch {epoch}, Accuracy: {100 * correct / total}%')
+        
+        # Step the learning rate scheduler
+        scheduler.step()
+
+def create_validation_dataset():
+    #TODO
+    pass
+
+def evaluate_model():
+    # Load the trained model
+    model_path = f'model_epoch_{epoch_amount-1}.pt'  # replace with your model path
+    model = CustomOPN()
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+
+    # Create a DataLoader for the test data
+    test_dataset = PreprocessedTemporalFourData(video_dataset, trainval='test')
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+    # Initialize the running accuracy
+    running_corrects = 0
+
+    # Iterate over the test data
+    for inputs, frame_order_labels, action_labels, video_name, _ in test_loader:
+        # Convert inputs to float
+        inputs = inputs.to(torch.float32)
+
+        # Change the order of dimensions to (batch_size, frames, channels, height, width)
+        inputs = inputs.permute(0, 1, 4, 2, 3)
+        # Concatenate frames and channels
+        inputs = inputs.contiguous().view(-1, inputs.shape[1]*inputs.shape[2], inputs.shape[3], inputs.shape[4])
+
+        # Make predictions
+        outputs = model(inputs)
+
+        # Get predicted class labels
+        _, predicted_labels = torch.max(outputs, 1)
+
+        # Update the running accuracy
+        running_corrects += torch.sum(predicted_labels == frame_order_labels.data)
+
+    # Calculate the final accuracy
+    accuracy = running_corrects.double() / len(test_loader.dataset)
+    print(f'Test Accuracy: {accuracy}')
+
+def visualize_network():
+    from torchviz import make_dot
+
+    # Assuming `inputs` is a batch of your input data
+    inputs = torch.randn(1, 4, 80, 80, 3)  # Adjust the size as needed
+
+    model = CustomOPN()
+
+    # Permute the dimensions to (batch_size, frames, channels, height, width)
+    inputs = inputs.permute(0, 1, 4, 2, 3)
+
+    # Flatten the frames and channels dimensions
+    inputs = inputs.contiguous().view(-1, inputs.shape[1]*inputs.shape[2], inputs.shape[3], inputs.shape[4])
+
+    # Ensure there is more than one value per channel
+    if inputs.shape[0] == 1:
+        inputs = inputs.repeat(2, 1, 1, 1)
+
+    outputs = model(inputs)
+
+    # Create the graph
+    dot = make_dot(outputs, params=dict(model.named_parameters()))
+
+    # Display the graph
+    dot.view()
 
 if __name__ == '__main__':
     main()
--- model.py
+++ model.py
@@ -1,17 +1,75 @@
-from azure.storage.blob import BlobServiceClient, ContainerClient, BlobPrefix
-import cv2
-import imageio.v3 as iio
-import torch
-import torch.nn as nn
-from torch.utils.data import Dataset
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.optim.lr_scheduler import MultiStepLR
-from PIL import Image
-import numpy as np
-import random
-import os
-import matplotlib.pyplot as plt
-from torchviz import make_dot
-from copy import deepcopy
-import time
+
+
+class CustomOPN(nn.Module):
+    def __init__(self):
+        super(CustomOPN, self).__init__()
+
+        # 1. FEATURE EXTRACTION
+        # Processes the input frames and extracts features, before flattening output to next stage.
+        self.conv_layers = nn.Sequential(
+        nn.Conv2d(4*3, 96, kernel_size=11, stride=4, padding=0),  # accepting frames*channels as input
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d(kernel_size=3, stride=2),
+        nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75),
+        nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2, groups=2),
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d(kernel_size=3, stride=2),
+        nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75),
+        nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm2d(384),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1, groups=2),
+        nn.BatchNorm2d(384),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1, groups=2),
+        nn.BatchNorm2d(256),
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d(kernel_size=3, stride=2)
+        )
+
+        # 2. PAIRWISE FEATURE EXTRACTION:
+        # fc6 layer processes flattened output, splits it into four parts, 
+        # and concatenates pairs to form new feature vectors. These vectors are then processed by the fc7 layers.
+        self.fc6 = nn.Linear(256 * 1 * 1, 1024)  # Assuming the input size from conv2d is (256, 1, 1)
+        self.bn6 = nn.BatchNorm1d(1024)
+        self.relu6 = nn.ReLU(inplace=True)
+        self.drop6 = nn.Dropout(0.5)
+
+        # The fc7 layers process the pairwise features
+        self.fc7_layers = nn.ModuleList()
+        for i in range(6):
+            self.fc7_layers.append(nn.Sequential(
+                nn.Linear(512, 512),
+                nn.BatchNorm1d(512),
+                nn.ReLU(inplace=True),
+                nn.Dropout(0.5)
+            ))
+
+        # 3. ORDER PREDICTION:
+        # fc8 layer processes concatenated feature vectors from previous stage and outputs final predictions.
+        self.fc8 = nn.Linear(512*6, 12)  # 6 is the number of concatenated layers
+
+    def forward(self, x):
+        # Feature extraction
+        x = self.conv_layers(x)
+        x = x.view(x.size(0), -1)  # Flatten the tensor
+
+        # Pairwise feature extraction
+        x = self.fc6(x)
+        x = self.bn6(x)
+        x = self.relu6(x)
+        x = self.drop6(x)
+        x1, x2, x3, x4 = x.chunk(4, dim=1)  # Slice the tensor into 4 parts
+        x_concat = [torch.cat((x1, x2), dim=1), torch.cat((x2, x3), dim=1), torch.cat((x3, x4), dim=1),
+                torch.cat((x1, x3), dim=1), torch.cat((x2, x4), dim=1), torch.cat((x1, x4), dim=1)]
+        
+        out = []
+        for i in range(6):
+            out_i = self.fc7_layers[i](x_concat[i])
+            out.append(out_i)
+
+        # Order prediction. Predicts the order of the frames.
+        out = torch.cat(out, dim=1)  # Concatenate along the channel dimension
+        #print(out.shape)
+        out = self.fc8(out)
+        return out
